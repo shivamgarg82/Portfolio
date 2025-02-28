e5f51a9a5715d87c0df416b17783ab5d26c266f4
shivamgarg82 <ngarg82871@gmail.com>
2025-02-28 16:52:44 +0530

    Create script.js
    Signed-off-by: shivamgarg82 <ngarg82871@gmail.com>

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn =document.querySelector(' .b');
+let ul =document.querySelector('.u');
+
+ btn.addEventListener('click', function my()
+{
+  if(ul.style.display=="block"){
+     ul.style.display="none";
+}
+else{
+  ul.style.display="block";
+}   
+});
+
+let btn=document.getElementById('btn');
